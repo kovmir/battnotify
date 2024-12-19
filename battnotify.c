e48f281a0dacf029fe86f52c3af3cef07fddb5ae
Ivan Kovmir <i@kovmir.eu>
2024-12-19 19:02:30 +0100

    consistent variable naming

--- battnotify.c
+++ battnotify.c
@@ -11,9 +11,8 @@
 
 /* Constants and Macros */
 #define BUF_SIZE 16
-
 #define AC_STATE_CHARGING 1
-#define NOTIFY_APP_NAME "battnotify"
+#define NTFN_APP_NAME "battnotify"
 
 /* Function Prototypes */
 /* Read battery charge percentage [0-100].
@@ -34,13 +33,16 @@ read_num_file(const char *file_path, int *num)
 	int fd;
 	ssize_t n_read;
 	char buf[BUF_SIZE];
+
 	fd = open(file_path, O_RDONLY);
 	if (fd < 0)
 		return 1;
+
 	n_read = read(fd, buf, BUF_SIZE);
 	if (n_read < 0)
 		return 1;
 	buf[n_read] = 0;
+
 	*num = (int)strtol(buf, NULL, 10);
 	close(fd);
 	return 0;
@@ -61,15 +63,16 @@ is_charging(int *state)
 int
 main(void)
 {
-	int batt_percent;
-	int ac_status;
-	char buf[BUF_SIZE];
-	NotifyNotification *batt_notifcn;
+	int batt_percent;   /* Battery charge level in percent. */
+	int ac_status;      /* Power cord presence. */
+	char buf[BUF_SIZE]; /* String to be sent to notification. */
+	NotifyNotification *batt_ntfn; /* Notification handle. */
 
-	notify_init(NOTIFY_APP_NAME);
-	batt_notifcn = notify_notification_new(NULL, NULL, NULL);
-	notify_notification_set_urgency(batt_notifcn, ntfy_urgency_level);
-	notify_notification_set_timeout(batt_notifcn, TIMEOUT);
+	/* Notification setup code. */
+	notify_init(NTFN_APP_NAME);
+	batt_ntfn = notify_notification_new(NULL, NULL, NULL);
+	notify_notification_set_urgency(batt_ntfn, ntfn_urgency_level);
+	notify_notification_set_timeout(batt_ntfn, ntfn_timeout);
 
 	for (;; sleep(polling_delay)) {
 		if (is_charging(&ac_status)) {
@@ -77,19 +80,20 @@ main(void)
 				"failed to access %s\n", ac_path);
 			return 1;
 		}
-
 		if (ac_status == AC_STATE_CHARGING)
 			continue;
-		if (batt_percent > batt_warn_percent) {
-			continue;
-		}
 
 		if (get_batt_percentage(&batt_percent)) {
 			fprintf(stderr, "failed to access %s\n", batt_path);
 			return 1;
 		}
+		if (batt_percent > batt_warn_percent) {
+			continue;
+		}
+
 		snprintf(buf, BUF_SIZE, "%d%%", batt_percent);
-		notify_notification_update(batt_notifcn, ntfn_title, buf, NULL);
-		notify_notification_show(batt_notifcn, NULL);
+
+		notify_notification_update(batt_ntfn, ntfn_title, buf, NULL);
+		notify_notification_show(batt_ntfn, NULL);
 	}
 }
