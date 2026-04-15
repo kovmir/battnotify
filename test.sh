b1907c4d68cb5d98eb25babdeb22f28c531bedc5
Ivan Kovmir <i@kovmir.eu>
2026-04-15 20:19:42 +0200

    Abort with error exit code if unit test not passed

--- test.sh
+++ test.sh
@@ -5,6 +5,9 @@ echo 0  > "$2"
 _output=$(./battnotify)
 if [[ "$_output" == '14%' ]]; then
 	echo '1 OK'
+else
+	echo '1 NOT OK'
+	exit 1
 fi
 
 echo 16 > "$1"
@@ -12,6 +15,9 @@ echo 0  > "$2"
 _output=$(./battnotify)
 if [[ "$_output" == '' ]]; then
 	echo '2 OK'
+else
+	echo '2 NOT OK'
+	exit 1
 fi
 
 echo 14 > "$1"
@@ -19,6 +25,9 @@ echo 1  > "$2"
 _output=$(./battnotify)
 if [[ "$_output" == '' ]]; then
 	echo '3 OK'
+else
+	echo '3 NOT OK'
+	exit 1
 fi
 
 echo 16 > "$1"
@@ -26,4 +35,7 @@ echo 1  > "$2"
 _output=$(./battnotify)
 if [[ "$_output" == '' ]]; then
 	echo '4 OK'
+else
+	echo '4 NOT OK'
+	exit 1
 fi
